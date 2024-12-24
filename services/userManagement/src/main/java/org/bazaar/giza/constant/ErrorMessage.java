63159b3965270babe51249617c598cc9c4423387
OEH808 <omarehababdelaziz8@gmail.com>
2024-12-24 17:28:03 +0200

    Implemented custom errors for BazaarUser

---
+++ services/userManagement/src/main/java/org/bazaar/giza/constant/ErrorMessage.java
@@ -0,0 +1,7 @@
+package org.bazaar.giza.constant;
+
+public class ErrorMessage {
+    public final static String USER_ID_NOT_FOUND = "A user with the given id does not exist.";
+    public final static String DUPLICATE_EMAIL = "A user with the given email already exists.";
+    public final static String DUPLICATE_PHONE_NUMBER = "A user with the given phone number already exists.";
+}
--- services/userManagement/src/main/java/org/bazaar/giza/user/service/BazaarUserServiceImpl.java
+++ services/userManagement/src/main/java/org/bazaar/giza/user/service/BazaarUserServiceImpl.java
@@ -2,7 +2,9 @@ package org.bazaar.giza.user.service;
 
 import java.util.Optional;
 
+import org.bazaar.giza.constant.ErrorMessage;
 import org.bazaar.giza.user.entity.BazaarUser;
+import org.bazaar.giza.user.exception.BazaarUserException;
 import org.bazaar.giza.user.repo.BazaarUserRepo;
 import org.springframework.stereotype.Service;
 
@@ -15,13 +17,19 @@ public class BazaarUserServiceImpl implements BazaarUserService {
 
     @Override
     public BazaarUser createUser(BazaarUser bazaarUser) {
+
+        boolean emailExists = bazaarUserRepo.findByEmail(bazaarUser.getEmail()).isPresent();
+        boolean phoneExists = bazaarUserRepo.findByPhoneNumber(bazaarUser.getPhoneNumber()).isPresent();
         // Duplicate email or phone number are not allowed
-        if (bazaarUserRepo.findByEmail(bazaarUser.getEmail()).isPresent()
-                || bazaarUserRepo.findByPhoneNumber(bazaarUser.getPhoneNumber()).isPresent()) {
-            // FIXME: Replace with custom exception
-            throw new RuntimeException();
+        if (emailExists && phoneExists) {
+            throw new BazaarUserException(
+                    String.format("%s\n%s", ErrorMessage.DUPLICATE_EMAIL, ErrorMessage.DUPLICATE_PHONE_NUMBER));
+        } else if (emailExists) {
+            throw new BazaarUserException(ErrorMessage.DUPLICATE_EMAIL);
+        } else if (phoneExists) {
+            throw new BazaarUserException(ErrorMessage.DUPLICATE_PHONE_NUMBER);
         }
-        
+
         return bazaarUserRepo.save(bazaarUser);
     }
 
@@ -29,8 +37,7 @@ public class BazaarUserServiceImpl implements BazaarUserService {
     public BazaarUser getSingleUser(Long userId) {
         Optional<BazaarUser> bazaarUserOptional = bazaarUserRepo.findById(userId);
         if (bazaarUserOptional.isEmpty()) {
-            // FIXME: Replace with custom exception
-            throw new RuntimeException();
+            throw new BazaarUserException(ErrorMessage.USER_ID_NOT_FOUND);
         }
 
         return bazaarUserOptional.get();
