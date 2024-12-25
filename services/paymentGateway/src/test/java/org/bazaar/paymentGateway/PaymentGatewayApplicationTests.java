3397e7f20e7825d9ac43dfc88e1de32d48f068d2
OEH808 <omarehababdelaziz8@gmail.com>
2024-12-25 15:52:51 +0200

    Initialized Payment gateway

---
+++ services/paymentGateway/src/test/java/org/bazaar/paymentGateway/PaymentGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bazaar.paymentGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
