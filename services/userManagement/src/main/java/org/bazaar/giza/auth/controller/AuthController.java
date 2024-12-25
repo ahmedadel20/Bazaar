e60e90c8e1eee57f97c22659b72db6c8646a81c5
OEH808 <omarehababdelaziz8@gmail.com>
2024-12-25 14:04:24 +0200

    Added authorization requirements to certain end points

--- services/userManagement/src/main/java/org/bazaar/giza/auth/controller/AuthController.java
+++ services/userManagement/src/main/java/org/bazaar/giza/auth/controller/AuthController.java
@@ -4,6 +4,8 @@ import org.bazaar.giza.auth.dto.SignInRequestDTO;
 import org.bazaar.giza.auth.dto.UserRegisterationDTO;
 import org.bazaar.giza.auth.service.JwtService;
 import org.bazaar.giza.auth.service.UserRolesService;
+import org.bazaar.giza.constant.Roles;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,12 +28,15 @@ public class AuthController {
     }
 
     @PostMapping("/registerManager")
+    @PreAuthorize("hasAuthority('" + Roles.ADMIN + "') or " +
+            "(hasAuthority('" + Roles.MANAGER + "')")
     public String registerManager(@RequestBody UserRegisterationDTO dto) {
         userRolesService.registerManager(dto);
         return jwtService.authenticateAndGetToken(dto.email(), dto.password());
     }
 
     @PostMapping("/registerAdmin")
+    @PreAuthorize("hasAuthority('" + Roles.ADMIN + "')")
     public String registerAdmin(@RequestBody UserRegisterationDTO dto) {
         userRolesService.registerAdmin(dto);
         return jwtService.authenticateAndGetToken(dto.email(), dto.password());
