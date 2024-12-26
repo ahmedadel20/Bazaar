e6e4d18cb1b055b9a2490924839eddc5ee1fde34
Ahmed Abdelkawy <48753857+ahmedadel20@users.noreply.github.com>
2024-12-26 11:31:46 +0200

    Update TransactionMapper.java
    Unified Mapper with @Component

--- services/order-management/src/main/java/org/bazaar/giza/transaction/mapper/TransactionMapper.java
+++ services/order-management/src/main/java/org/bazaar/giza/transaction/mapper/TransactionMapper.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 
 import java.sql.Date;
 
-@Service
+@Component
 @RequiredArgsConstructor
 public class TransactionMapper {
     private final OrderMapper orderMapper;
