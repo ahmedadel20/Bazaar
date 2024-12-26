5b131f28e4afc272fc154c895307c5f73bd79869
OEH808 <omarehababdelaziz8@gmail.com>
2024-12-26 16:48:42 +0200

    Initialized product catalogue service

---
+++ services/productCatalogue/src/test/java/org/bazaar/productCatalogue/ProductCatalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bazaar.productCatalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
