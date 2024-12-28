56aaea757206ca7164bc235a7baf8fd2f000b8ef
Ahmed Adel <ahmedadel99991@gmail.com>
2024-12-28 16:51:38 +0200

    updated NotificationService.

--- services/notification/src/main/java/org/bazaar/giza/notification/service/NotificationService.java
+++ services/notification/src/main/java/org/bazaar/giza/notification/service/NotificationService.java
@@ -4,6 +4,6 @@ import jakarta.mail.MessagingException;
 import org.bazaar.giza.notification.dto.NotificationDto;
 
 public interface NotificationService {
-    public void processEmailMessage(NotificationDto notificationDto);
-    public void sendEmail(String to, String subject, String body) throws MessagingException;
+    void processEmailMessage(NotificationDto notificationDto);
+    void sendEmail(String to, String subject, String body) throws MessagingException;
 }
