a957334d56951690a92d79a8dadbb2b164ef7a7c
OEH808 <omarehababdelaziz8@gmail.com>
2024-12-29 14:35:05 +0200

    Updated InventoryClient URL

--- services/productCatalogue/src/main/java/org/bazaar/productCatalogue/client/InventoryClient.java
+++ services/productCatalogue/src/main/java/org/bazaar/productCatalogue/client/InventoryClient.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @FeignClient(name = "inventory", url = "${inventory.service.url}")
 public interface InventoryClient {
-    @GetMapping("/api/v1/productsIds")
+    @GetMapping("/api/v1/products/bycategories")
     List<Long> getProductById(List<String> categories);
 }
