f73282257ce2121986f2af7dbd0ba742262fe63c
OEH808 <omarehababdelaziz8@gmail.com>
2025-01-05 14:35:47 +0200

    Updated validation for creating a customer

--- services/userManagement/src/main/java/org/bazaar/giza/customer/repo/CustomerRepository.java
+++ services/userManagement/src/main/java/org/bazaar/giza/customer/repo/CustomerRepository.java
@@ -2,6 +2,10 @@ package org.bazaar.giza.customer.repo;
 
 import org.bazaar.giza.customer.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findByEmail(String email);
+
+    Optional<Customer> findByPhoneNumber(String phoneNumber);
 }
--- services/userManagement/src/main/java/org/bazaar/giza/customer/service/CustomerServiceImpl.java
+++ services/userManagement/src/main/java/org/bazaar/giza/customer/service/CustomerServiceImpl.java
@@ -11,6 +11,7 @@ import org.bazaar.giza.customer.entity.Address;
 import org.bazaar.giza.customer.entity.Customer;
 import org.bazaar.giza.customer.exception.CustomerException;
 import org.bazaar.giza.customer.repo.CustomerRepository;
+import org.bazaar.giza.user.repo.BazaarUserRepo;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,12 +21,15 @@ import java.util.Set;
 @Service
 @RequiredArgsConstructor
 public class CustomerServiceImpl implements CustomerService {
-    private final CustomerRepository repository;
+    private final BazaarUserRepo bazaarUserRepo;
+    private final CustomerRepository customerRepo;
     private final CustomerMapper mapper;
 
     public Customer createCustomer(Customer customer) {
-        // FIXME: Add Validation
-        return repository.save(customer);
+        customer.setUserId(null);
+        validateCustomer(customer);
+
+        return customerRepo.save(customer);
     }
 
     public CustomerResponse getSingleCustomer(Long customerId) {
@@ -33,14 +37,14 @@ public class CustomerServiceImpl implements CustomerService {
     }
 
     public List<CustomerResponse> getAllCustomers() {
-        return repository.findAll().stream().map(mapper::toCustomerResponse).toList();
+        return customerRepo.findAll().stream().map(mapper::toCustomerResponse).toList();
     }
 
     @Override
     public Set<Address> addAddress(Long customerId, AddressRequest addressRequest) {
         Customer customer = searchId(customerId);
         customer.addAddress(mapper.toAddress(addressRequest));
-        customer = repository.save(customer);
+        customer = customerRepo.save(customer);
         return customer.getAddresses();
     }
 
@@ -48,18 +52,20 @@ public class CustomerServiceImpl implements CustomerService {
     public Set<Address> removeAddress(Long customerId, AddressRequest addressRequest) {
         Customer customer = searchId(customerId);
         customer.removeAddress(mapper.toAddress(addressRequest));
-        customer = repository.save(customer);
+        customer = customerRepo.save(customer);
         return customer.getAddresses();
     }
 
     public CustomerResponse updateCustomer(CustomerRequest request) {
         var customer = searchId(request.id());
-        customer = repository.save(mapper.toCustomer(request));
+        validateCustomer(customer);
+
+        customer = customerRepo.save(mapper.toCustomer(request));
         return mapper.toCustomerResponse(customer);
     }
 
     public String deleteCustomer(Long customerId) {
-        repository.delete(searchId(customerId));
+        customerRepo.delete(searchId(customerId));
         return "Customer deleted";
     }
 
@@ -68,11 +74,28 @@ public class CustomerServiceImpl implements CustomerService {
         if (id == null) {
             throw new CustomerException(ErrorMessage.ID_CANNOT_BE_NULL);
         }
-        Optional<Customer> customerOptional = repository.findById(id);
+        Optional<Customer> customerOptional = customerRepo.findById(id);
         if (customerOptional.isEmpty()) {
             throw new CustomerException(ErrorMessage.CUSTOMER_ID_NOT_FOUND);
         }
 
         return customerOptional.get();
+    }
+
+    private void validateCustomer(Customer customer) {
+        boolean emailExists = bazaarUserRepo.findByEmail(customer.getEmail()).isPresent()
+                || customerRepo.findByEmail(customer.getEmail()).isPresent();
+        boolean phoneExists = bazaarUserRepo.findByPhoneNumber(customer.getPhoneNumber()).isPresent()
+                || customerRepo.findByPhoneNumber(customer.getPhoneNumber()).isPresent();
+
+        // Duplicate email or phone number are not allowed
+        if (emailExists && phoneExists) {
+            throw new CustomerException(
+                    String.format("%s\n%s", ErrorMessage.DUPLICATE_EMAIL, ErrorMessage.DUPLICATE_PHONE_NUMBER));
+        } else if (emailExists) {
+            throw new CustomerException(ErrorMessage.DUPLICATE_EMAIL);
+        } else if (phoneExists) {
+            throw new CustomerException(ErrorMessage.DUPLICATE_PHONE_NUMBER);
+        }
     }
 }
