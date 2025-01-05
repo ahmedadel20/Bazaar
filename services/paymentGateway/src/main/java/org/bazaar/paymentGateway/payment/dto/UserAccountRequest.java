6857074921e4b4b8573dd3ecda80549d84e70e4c
abdalla_maged <abdalla.maged95@gmail.com>
2025-01-05 18:55:15 +0200

    Added swagger to payment gateway

--- services/paymentGateway/src/main/java/org/bazaar/paymentGateway/payment/dto/UserAccountRequest.java
+++ services/paymentGateway/src/main/java/org/bazaar/paymentGateway/payment/dto/UserAccountRequest.java
@@ -4,16 +4,25 @@ import java.math.BigDecimal;
 
 import org.bazaar.paymentGateway.constant.ValidationMessage;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 public record UserAccountRequest(
-        @NotBlank(message = "email"
-                + ValidationMessage.NOT_BLANK) @Email(message = ValidationMessage.INVALID_EMAIL, regexp = ValidationMessage.EMAIL_REGEX) String email,
-        @NotBlank(message = "password" + ValidationMessage.NOT_BLANK) String password,
-        @NotNull(message = "amountOfMoney" + ValidationMessage.NOT_NULL) @Positive(message = "amountOfMoney"
-                + ValidationMessage.POSITIVE) BigDecimal amountOfMoney) {
-
-}
+        @NotBlank(message = "email" + ValidationMessage.NOT_BLANK)
+        @Email(message = ValidationMessage.INVALID_EMAIL, regexp = ValidationMessage.EMAIL_REGEX)
+        @Schema(requiredProperties = {"Can't be blank", "Must be a valid email"})
+        String email,
+        
+        @NotBlank(message = "password" + ValidationMessage.NOT_BLANK)
+        @Schema(requiredProperties = {"Can't be blank"})
+        String password,
+        
+        @NotNull(message = "amountOfMoney" + ValidationMessage.NOT_NULL)
+        @Positive(message = "amountOfMoney" + ValidationMessage.POSITIVE)
+        @Schema(requiredProperties = {"Can't be null", "Must be positive integer"})
+        BigDecimal amountOfMoney
+)
+{}
